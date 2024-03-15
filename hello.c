9960c5ba742528e842112e85a8dc647e7d5a5a83
gawon Lee <joomong_@naver.com>
2024-03-15 10:48:58 +0900

    Isabel made hello.c

--- hello.c
+++ hello.c
@@ -2,14 +2,11 @@
 
 int main()
 {
-<<<<<<< HEAD
-  printf("hello world");
+
   printf("ok\n");
   printf("Nope\n");
 
-=======
-prinf("hello world");
->>>>>>> parent of 10bebf0 (I made hello.c)
+
 
 return 0;
 }
