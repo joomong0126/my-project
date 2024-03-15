64ae8a049899918d1122e8751bde2f73845fbd86
gawon Lee <joomong_@naver.com>
2024-03-15 10:46:50 +0900

    We made hello.c

--- hello.c
+++ hello.c
@@ -2,11 +2,15 @@
 
 int main()
 {
+<<<<<<< HEAD
   printf("hello world");
-  printf("ok");
-  printf("Nope");
+  printf("ok\n");
+  printf("Nope\n");
 
+=======
+prinf("hello world");
+>>>>>>> parent of 10bebf0 (I made hello.c)
 
- return 0;
+return 0;
 }
 
